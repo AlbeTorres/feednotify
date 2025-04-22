efe3f47f93e6ee81412acb900bc6d46dda6e269a
AlbeTorres <albertocorreoficial@gmail.com>
2025-04-21 20:40:29 -0400

    Add Playstation Blog RSS feed and enhance readRssFeeds function

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,14 @@ import { readRssFeeds } from "./readers/rssreader";
 
 const feeds = [
   {
-    id: '1',
-    type: 'rss',
-    name: 'Dev.to',
-    url: 'https://dev.to/feed',
-  },
-  {
-    id: '2',
-    type: 'rss',
-    name: 'CSS-Tricks',
-    url: 'https://css-tricks.com/feed/',
+    "id": "Playstation_Blog",
+    "type": "rss",
+    "name": "Playstation Blog",
+    "url": "https://blog.playstation.com/feed"
   },
 ];
 
-readRssFeeds(feeds);
+const yesterday = new Date();
+yesterday.setDate(yesterday.getDate() - 1);
+
+readRssFeeds(feeds, yesterday, "https://blog.playstation.com/2023/10/02/ps5-system-software-update-23-02-01-00-now-live/");
--- src/readers/rssreader.ts
+++ src/readers/rssreader.ts
@@ -9,19 +9,54 @@ type Feed = {
 
 const parser = new Parser();
 
-export async function readRssFeeds(feeds: Feed[]) {
+export async function readRssFeeds(feeds: Feed[], pubDateThreshold: Date, itemLinkThreshold: string) {
   const rssFeeds = feeds.filter(feed => feed.type === 'rss');
 
   for (const feed of rssFeeds) {
     try {
       const parsed = await parser.parseURL(feed.url);
       console.log(`📥 Feed: ${feed.name}`);
-      parsed.items?.slice(0, 3).forEach((item, i) => {
-        console.log(`  ${i + 1}. ${item.title}`);
+      console.log(parsed.items[0])
+
+      //Check if the feed has items 
+      if (!parsed.items || parsed.items.length === 0) {
+        console.log('  No hay artículos en este feed.');
+        return;
+      }
+
+      const filteredItems = filterItems(parsed.items, pubDateThreshold, itemLinkThreshold);
+
+      filteredItems.slice(0, 3).forEach((item, i) => {
+        console.log(`  ${i + 1}. ${item.title}`); 
         console.log(`     ${item.link}`);
       });
+
+      if (filteredItems.length === 0) {
+        console.log('  No hay artículos nuevos desde la última vez que se leyó este feed.');
+      } else {
+        console.log(`  ${filteredItems.length} artículos nuevos desde la última vez que se leyó este feed.`);
+      }
     } catch (error) {
       console.error(`❌ Error leyendo feed "${feed.name}":`, error);
     }
   }
+}
+
+function filterItems(parsedItems: any[], pubDateThreshold: Date, itemLinkThreshold: string):any[] {
+
+   //Check if the feed has a pubDate property
+   //TODO: if the feed has no pubDate property, filter by the link property, take the items before the item with the same link property as itemLinkThreshold
+
+  parsedItems.filter(item => {
+   
+    if (!item.pubDate){
+    
+
+    }else {
+      const itemDate = new Date(item.pubDate);
+      return !isNaN(itemDate.getTime()) && itemDate > pubDateThreshold;
+    }
+  });
+
+  return []
 }
